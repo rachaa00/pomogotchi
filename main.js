b0f2ddcb7d3ba85ac2c5836c6456e16476ac51d2
Annarachele Altorio <60370915+Annar00@users.noreply.github.com>
2026-01-28 11:42:49 +0000

    cleaned html adding evenlistener in js

--- main.js
+++ main.js
@@ -5,13 +5,14 @@
 
 
 const secondEl = document.getElementById('timer');
-// const minuteEl = document.getElementById("minute");
-// const hourEl = document.getElementById("hour");
+
 
 let second = Number(secondEl.innerText);
-// let minute = Number(minuteEl.innerText);
-// let hour = Number(hourEl.innerText); 
 
+const btn15 = document.getElementById('btn15');
+const btn30 = document.getElementById('btn30');
+const btn60 = document.getElementById('btn60'); 
+const btnStart = document.getElementById('btnStart');
 
 
 // this funciton only will give me the message after the timer is up , so based on the timer i select the function will show the message. 
@@ -53,120 +54,6 @@ function startCountdown(totalSeconds) {
 
 
 
-
-
-
-
-
-
-
-
-// function startTimer() { // i need to do a while loop for seconds, one for minutes and one for hours , all inside the same function 
-
-//     //tried while loop for hour
-//     let count_hour= 0;
-//     let delay_hour= 3600000;
-    
-//     while (count_hour<60) {
-//         setTimeout(() => {
-//             hour -=1;
-//             hourEl.innerText = "0" + hour;  
-//         }, delay_hour);
-//         delay_hour += 3600000;
-//         count_hour++;
-//     }
-
-//     let count_minute= 0;
-//     let delay_minute= 3540000;
-    
-//     while (count_minute<60) {
-//         setTimeout(() => {
-//             minute -=1;
-//             minuteEl.innerText = "0" + minute;  
-//         }, delay_minute);
-//         delay_minute += 3540000;
-//         count_minute++;
-//     }
-
-//     let delay_second = 1000 
-//     let count_second = 0
-
-//     while (count_second<60) {
-//         setTimeout(() => {
-//             second-=1;
-//             secondEl.innerText = second; 
-//         }, delay_second);
-//         delay_second += 1000;
-//         count_second++;
-//     }
-
-// }
-
-
-// console.log(typeof secondEl)
-
-
-
-
-
-
-
-// i want now the actual timer that shows the time 
-
-
-//function firstSecond() {
-//    let second = "1"
-//        setTimeout(() => {
-//    timeEl.innerText = second;
-//    }, 1000); 
-//}
-
-
-
-//for the seconds
-   // setTimeout(() => {
-   // time -=1;
-   // timeEl.innerText = "0"+ time; //i cannot use the "0" becasue when then is double number what do i do? for sure there is a way to show it properly
-   // console.log(time)
-   // }, 1000); 
-
-
-   // setTimeout(() => {
-   // time -=1;
-  //  timeEl.innerText = "0" + time; 
-  //  console.log(time)
-  //  }, 2000); 
-
-
-  //  setTimeout(() => {
-  // time -=1;
-  //  timeEl.innerText = "0" + time; 
-  //  console.log(time)
-   // }, 3000); 
-
-
-   // setTimeout(() => {
-  //  time -=1;
-   // timeEl.innerText = "0" + time; 
-  //  console.log(time)
-  //  }, 4000); 
-
-    
-   // setTimeout(() => {
-   // time -=1;
-  //  timeEl.innerText = "0" + time; 
-  //  console.log(time)
-  //  }, 5000); 
-
-
-
-      // // tried setInterval for the minutes
-    // const intervalId = setInterval(() => {
-    //     minute-=1;
-    //     minuteEl.innerText = "0" + minute;
-    //     console.log(minute);
-        
-    //     if (minute<=0) {
-    //         clearInterval(intervalId);
-    //     }
-    // }, 1000);
+btn15.addEventListener("click", () => startCountdown(15 * 60));
+btn30.addEventListener("click", () => startCountdown(30 * 60));
+btn60.addEventListener("click", () => startCountdown(60 * 60));
